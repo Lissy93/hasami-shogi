024ad39ffd88339c3966c3fc15ebac79681921cb
Alicia Sykes <Alicia@Alicias-Mac-mini.local>
2015-09-25 14:24:48 +0100

    Initial Commit

---
+++ Hasami Shogi/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Hasami Shogi
+//
+//  Created by Alicia Sykes on 25/09/2015.
+//  Copyright (c) 2015 Alicia Sykes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

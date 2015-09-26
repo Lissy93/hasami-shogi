1c3753850eb6d994c1a25dc8ef6aa0587d3cc054
Alicia Sykes <Alicia@Alicias-Mac-mini.local>
2015-09-26 15:06:46 +0100

    Added controller for game screen and hooked it up

--- Hasami Shogi/GameViewController.swift
+++ Hasami Shogi/GameViewController.swift
@@ -1,9 +1,31 @@
 //
-//  GameViewController.swift
+//  ViewController.swift
 //  Hasami Shogi
 //
-//  Created by Alicia Sykes on 26/09/2015.
-//  Copyright © 2015 Alicia Sykes. All rights reserved.
+//  Created by Alicia Sykes on 25/09/2015.
+//  Copyright (c) 2015 Alicia Sykes. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class GameViewController: UICollectionViewController {
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        for _ in 1...5 { // putting this in a loop, so MS Sam can read it 5 times when it's printed to console
+            print("omg something which doesn't yet do anything is vagualey slightly working!!");
+        }
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    
+}
+

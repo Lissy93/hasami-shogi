fa52a0522cecaa90950cfc041ea56c4e055aa838
Alicia Sykes <alicia@aliciasykes.com>
2015-09-28 09:30:09 +0100

    Modified AppDelegate to start on the game screen

--- Hasami Shogi/AppDelegate.swift
+++ Hasami Shogi/AppDelegate.swift
@@ -15,10 +15,15 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
 
-    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-        // Override point for customization after application launch.
+    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject : AnyObject]?) -> Bool {
+        window = UIWindow(frame: UIScreen.mainScreen().bounds)
+        window!.backgroundColor = UIColor.redColor()
+        window!.rootViewController = GameViewController() // Change this back to MainController soon
+        window!.makeKeyAndVisible()
+        
         return true
     }
+
 
     func applicationWillResignActive(application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.

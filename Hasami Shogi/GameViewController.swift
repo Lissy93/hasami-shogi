e65662957c75879dd7d4ef8ba456eb7967ecc13b
Alicia Sykes <alicia@aliciasykes.com>
2015-10-03 20:01:19 +0100

    Modified and refractored game constraints

--- Hasami Shogi/GameViewController.swift
+++ Hasami Shogi/GameViewController.swift
@@ -19,18 +19,22 @@ class GameViewController: UIViewController, UICollectionViewDelegateFlowLayout, 
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        // Create the layout for the Game Grid
         let layout: UICollectionViewFlowLayout = UICollectionViewFlowLayout()
         layout.sectionInset = UIEdgeInsets(top: 1, left: 1, bottom: 1, right: 1)
         layout.itemSize = CGSize(width: 31, height: 31)
         layout.minimumInteritemSpacing = 0
         layout.minimumLineSpacing = 1
         
+        // Create the Game Grid
         gameGrid = UICollectionView(frame: self.view.frame, collectionViewLayout: layout)
         gameGrid.dataSource = self
         gameGrid.delegate = self
         gameGrid.registerClass(UICollectionViewCell.self, forCellWithReuseIdentifier: "Cell")
         gameGrid.backgroundColor = UIColor.whiteColor()
-        
+        gameGrid.translatesAutoresizingMaskIntoConstraints = false
+
+        // Create the Game Score View
         let gameHeaderView = UIView()
         gameHeaderView.translatesAutoresizingMaskIntoConstraints = false
         let title: UITextField = UITextField (frame:CGRectMake(10, 25, 120, 20));
@@ -38,71 +42,23 @@ class GameViewController: UIViewController, UICollectionViewDelegateFlowLayout, 
         gameHeaderView.addSubview(title)
         
         
-        gameGrid.translatesAutoresizingMaskIntoConstraints = false
-
-        
-//        view.backgroundColor = UIColor(
-//            red: 0.9,
-//            green: 0.9,
-//            blue: 1,
-//            alpha: 1.0)
-        
-        //Add the views
+        // Add the Game Score View and the Game View to main view
         view.addSubview(gameHeaderView)
         view.addSubview(gameGrid)
-        
-        
-        let viewsDictionary = [
-            "view1":gameHeaderView,
-            "view2":gameGrid]
-        
-        let metricsDictionary = [
-            "view1Height": 50.0,
-            "view2Height":40.0,
-            "viewWidth": 290.0 ]
 
+        // Add Constraints
+        if let constraints = applyConstraints([ "view1":gameHeaderView, "view2":gameGrid]){
         
-        let view_constraint_H = NSLayoutConstraint.constraintsWithVisualFormat(
-            "H:|-[view2]",
-            options: NSLayoutFormatOptions(rawValue:0),
-            metrics: nil, views: viewsDictionary)
-        let view_constraint_V = NSLayoutConstraint.constraintsWithVisualFormat(
-            "V:|-36-[view1]-8-[view2]-0-|",
-            options: NSLayoutFormatOptions.AlignAllLeading,
-            metrics: nil, views: viewsDictionary)
+            view.addConstraints(constraints.mainConstraints_H)
+            view.addConstraints(constraints.mainConstraints_V)
         
-        view.addConstraints(view_constraint_H)
-        view.addConstraints(view_constraint_V)
+            gameHeaderView.addConstraints(constraints.headerConstraints_H)
+            gameHeaderView.addConstraints(constraints.headerConstraints_V)
         
-        
-        // Game Score View
-        let view1_constraint_H = NSLayoutConstraint.constraintsWithVisualFormat(
-            "H:[view1(viewWidth)]",
-            options: NSLayoutFormatOptions(rawValue:0),
-            metrics: metricsDictionary,
-            views: viewsDictionary)
-        let view1_constraint_V = NSLayoutConstraint.constraintsWithVisualFormat(
-            "V:[view1(view1Height)]",
-            options: NSLayoutFormatOptions(rawValue:0),
-            metrics: metricsDictionary,
-            views: viewsDictionary)
-        
-        gameHeaderView.addConstraints(view1_constraint_H)
-        gameHeaderView.addConstraints(view1_constraint_V)
-        // Game Grid View
-        let view2_constraint_H = NSLayoutConstraint.constraintsWithVisualFormat(
-            "H:[view2(viewWidth)]",
-            options: NSLayoutFormatOptions(rawValue:0),
-            metrics: metricsDictionary,
-            views: viewsDictionary)
-        let view2_constraint_V = NSLayoutConstraint.constraintsWithVisualFormat(
-            "V:[view2(>=view2Height)]",
-            options: NSLayoutFormatOptions(rawValue:0),
-            metrics: metricsDictionary, 
-            views: viewsDictionary)
-        
-        gameGrid.addConstraints(view2_constraint_H)
-        gameGrid.addConstraints(view2_constraint_V)
+            gameGrid.addConstraints(constraints.gameConstraints_H)
+            gameGrid.addConstraints(constraints.gameConstraints_V)
+            
+        }
         
     }
     
@@ -148,7 +104,6 @@ class GameViewController: UIViewController, UICollectionViewDelegateFlowLayout, 
         let grid = UICollectionView();
         grid
     }
-
     
 }
 
@@ -175,5 +130,64 @@ struct Player {
 enum PlayerNum {
     case player1
     case player2
+}
+
+
+func applyConstraints(viewsDictionary: Dictionary<String,UIView>) -> (
+    mainConstraints_H: [NSLayoutConstraint], mainConstraints_V: [NSLayoutConstraint],
+    headerConstraints_H: [NSLayoutConstraint], headerConstraints_V: [NSLayoutConstraint],
+    gameConstraints_H: [NSLayoutConstraint], gameConstraints_V: [NSLayoutConstraint])? {
+        
+        var mc_H, mc_V, hc_H, hc_V, gc_H, gc_V: [NSLayoutConstraint] // Results to return
+        
+        let metricsDictionary = [
+            "view1Height": 50.0,
+            "view2Height":40.0,
+            "viewWidth": 290.0 ]
+        
+        let view_constraint_H = NSLayoutConstraint.constraintsWithVisualFormat(
+            "H:|-[view2]",
+            options: NSLayoutFormatOptions(rawValue:0),
+            metrics: nil, views: viewsDictionary)
+        let view_constraint_V = NSLayoutConstraint.constraintsWithVisualFormat(
+            "V:|-36-[view1]-8-[view2]-0-|",
+            options: NSLayoutFormatOptions.AlignAllLeading,
+            metrics: nil, views: viewsDictionary)
+        
+        mc_H = view_constraint_H
+        mc_V = view_constraint_V
+        
+        
+        // Game Score View Contstraints
+        let view1_constraint_H = NSLayoutConstraint.constraintsWithVisualFormat(
+            "H:[view1(viewWidth)]",
+            options: NSLayoutFormatOptions(rawValue:0),
+            metrics: metricsDictionary,
+            views: viewsDictionary)
+        let view1_constraint_V = NSLayoutConstraint.constraintsWithVisualFormat(
+            "V:[view1(view1Height)]",
+            options: NSLayoutFormatOptions(rawValue:0),
+            metrics: metricsDictionary,
+            views: viewsDictionary)
+        
+        hc_H = view1_constraint_H
+        hc_V = view1_constraint_V
+        
+        // Game Grid View Constraints
+        let view2_constraint_H = NSLayoutConstraint.constraintsWithVisualFormat(
+            "H:[view2(viewWidth)]",
+            options: NSLayoutFormatOptions(rawValue:0),
+            metrics: metricsDictionary,
+            views: viewsDictionary)
+        let view2_constraint_V = NSLayoutConstraint.constraintsWithVisualFormat(
+            "V:[view2(>=view2Height)]",
+            options: NSLayoutFormatOptions(rawValue:0),
+            metrics: metricsDictionary,
+            views: viewsDictionary)
+        
+        gc_H = view2_constraint_H
+        gc_V = view2_constraint_V
+        
+        return (mc_H, mc_V, hc_H, hc_V, gc_H, gc_V)
 }
 

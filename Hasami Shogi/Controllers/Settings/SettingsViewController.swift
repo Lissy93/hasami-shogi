23a23c1c5781f9894f0efd020648ef53084d78db
Alicia Sykes <alicia@aliciasykes.com>
2015-10-19 20:45:08 +0100

    Form elements for settings page

---
+++ Hasami Shogi/Controllers/Settings/SettingsViewController.swift
@@ -0,0 +1,15 @@
+
+
+import UIKit
+
+class SettingsViewController: UITableViewController {
+   
+    
+    @IBOutlet weak var settingsTableView: UITableView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
